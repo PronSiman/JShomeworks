61b8c89ef395d87d4fc1f450c932a54c74252c96
Alex_vlasov1988 <Alex_vlasov@i.ua>
2018-03-01 23:06:35 +0200

    third commit

--- Module_03JS_01/JS/script.js
+++ Module_03JS_01/JS/script.js
@@ -5,10 +5,12 @@ const arr = alphabet.split('');
 const firstKeyboardLine = arr.slice(0, arr.indexOf('a'));
 const secondKeyboardLine = arr.slice(arr.indexOf('a'), arr.indexOf('z'));
 const thirdKeyboardLine = arr.slice(arr.indexOf('z'));
+const keyboard = [firstKeyboardLine, secondKeyboardLine, thirdKeyboardLine];
 
-const hello = secondKeyboardLine[5] + firstKeyboardLine[2] + secondKeyboardLine[8] + secondKeyboardLine[8] + firstKeyboardLine[8];
-const js = secondKeyboardLine[6] + secondKeyboardLine[0] + thirdKeyboardLine[3] + secondKeyboardLine[0] + secondKeyboardLine[1] + thirdKeyboardLine[2] + firstKeyboardLine[3] + firstKeyboardLine[7] + firstKeyboardLine[9] + firstKeyboardLine[4];
-const trainer = firstKeyboardLine[4] + firstKeyboardLine[3] + secondKeyboardLine[0] + firstKeyboardLine[7] + thirdKeyboardLine[5] + firstKeyboardLine[2] + firstKeyboardLine[3];
+
+const hello = keyboard[1][5] + keyboard[0][2] + keyboard[1][8] + keyboard[1][8] + keyboard[0][8];
+const js = keyboard[1][6] + keyboard[1][0] + keyboard[2][3] + keyboard[1][0] + keyboard[1][1] + keyboard[2][2] + keyboard[0][3] + keyboard[0][7] + keyboard[0][9] + keyboard[0][4];
+const trainer = keyboard[0][4] + keyboard[0][3] + keyboard[1][0] + keyboard[0][7] + keyboard[2][5] + keyboard[0][2] + keyboard[0][3];
 
 console.log(hello);
 console.log(js);
