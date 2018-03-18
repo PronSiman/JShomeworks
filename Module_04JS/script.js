5c85e1e6d256a05025c29c4876b3f90414de74dc
Alex_vlasov1988 <Alex_vlasov@i.ua>
2018-03-18 14:45:14 +0200

    5th commit

--- Module_04JS/script.js
+++ Module_04JS/script.js
@@ -16,7 +16,7 @@ function addKeyboardLayout(string) {
 function getRandCharInRow() {
 
 
-    let numberString = +prompt('Введите номер строки на клавиатуре - число от  1 до 3') - 1;
+    const numberString = +prompt('Введите номер строки на клавиатуре - число от  1 до 3') - 1;
 
 
     if (numberString <= 2 && numberString >= 0) {
@@ -24,7 +24,7 @@ function getRandCharInRow() {
         const random = Math.floor(Math.random() * keyboard[numberString].length);
         alert(`Случайный символ из строки ${numberString+1}, это - ${keyboard[numberString][random] } `);
     } else {
-        // alert('Введите число от 1 до 3');
+
         numberString = +prompt('Введите номер строки на клавиатуре - число от  1 до 3') - 1;
     }
 
