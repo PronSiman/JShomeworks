cc26e029501f2ff1f08416dbee8a667940f8d88f
Alex_vlasov1988 <Alex_vlasov@i.ua>
2018-03-18 15:03:18 +0200

    5th commit

--- Module_04JS/script.js
+++ Module_04JS/script.js
@@ -1,46 +1,45 @@
 const alphabet = 'qwertyuiop[]asdfghjkl;\'zxcvbnm,./'
 
-const keyboard = addKeyboardLayout(alphabet);
-
 function addKeyboardLayout(string) {
     const keyboardArr = string.split('');
     const keyboardRows = [keyboardArr.slice(0, keyboardArr.indexOf("a")), keyboardArr.slice(keyboardArr.indexOf("a"), keyboardArr.indexOf("z")), keyboardArr.slice(keyboardArr.indexOf("z"))];
 
     return keyboardRows;
 }
+const keyboard = addKeyboardLayout(alphabet);
 
+let numberString;
 
-
-
-
-function getRandCharInRow() {
-
-
-    const numberString = +prompt('Введите номер строки на клавиатуре - число от  1 до 3') - 1;
-
-
+do {
+    numberString = +prompt(`Введите число от 1 до 3`) - 1;
     if (numberString <= 2 && numberString >= 0) {
-
-        const random = Math.floor(Math.random() * keyboard[numberString].length);
-        alert(`Случайный символ из строки ${numberString+1}, это - ${keyboard[numberString][random] } `);
-    } else {
-
-        numberString = +prompt('Введите номер строки на клавиатуре - число от  1 до 3') - 1;
+        const randomLetterIndex = getRandCharInRow(numberString)
+        alert(`Случайный символ из строки ${numberString+1}, это - ${keyboard[numberString][randomLetterIndex]} `);
+    } else if (confirm('Введите число от 1 до 3'));
+    else {
+        break;
     }
+} while (numberString > 2 || numberString < 0)
 
-
+function getRandCharInRow(randomLetterIndex) {
+    const randomIndex = Math.floor(Math.random() * keyboard[randomLetterIndex].length);
+    return randomIndex;
 }
-
-getRandCharInRow();
-
 
 function getRandCharInAlph(keyboard) {
 
-    let randomRow = Math.floor(Math.random() * keyboard.length);
-    let randomLetterIndex = Math.floor(Math.random() * keyboard[randomRow].length);
-    let randomLetter = keyboard[randomRow][randomLetterIndex];
-
+    const randomRow = Math.floor(Math.random() * keyboard.length);
+    const randomLetterIndex = getRandCharInRow(randomRow)
+    const randomLetter = keyboard[randomRow][randomLetterIndex];
     return randomLetter;
 }
 
 alert(`Случайный символ клавиатурных строк это  - "${getRandCharInAlph(keyboard)}"`);
+
+
+
+
+
+
+
+// прошлый код
