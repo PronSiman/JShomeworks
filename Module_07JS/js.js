284b3d46017adf5d9c60a4a5d5af809014656f7a
Alex_vlasov1988 <Alex_vlasov@i.ua>
2018-05-05 22:02:06 +0300

    second commit
    loop was changed to "for", changed way of dom adding

--- Module_07JS/js.js
+++ Module_07JS/js.js
@@ -5,27 +5,19 @@ function createLayout(alphabet) {
     const arr = alphabet.split('');
     const keyboard = [arr.slice(0, arr.indexOf("a")), arr.slice(arr.indexOf("a"), arr.indexOf("z")), arr.slice(arr.indexOf("z"))];
 
-    const row = document.querySelector(".row")
+    const wrapper = document.querySelector(".wrapper");
 
-    let k = 0;
-    keyboard.forEach(element => {
-        let currentKeys = document.createElement('ul');
-        currentKeys.classList.add('keys');
-        row.appendChild(currentKeys);
-        let i = 0;
-        keyboard[k].forEach(element => {
-            let currentLetter = document.createElement('li');
-            currentLetter.classList.add("key-letter")
-            currentLetter.textContent = keyboard[k][i];
-            currentKeys.appendChild(currentLetter);
-            i++;
+    let rows = '';
+    let buttons = '';
+    for (i = 0; i < keyboard.length; i++) {
+        for (k = 0; k < keyboard[i].length; k++) {
+            buttons += `<button class="key-button">${keyboard[i][k]}</button>`
+        }
+        rows += `<div class="key-row">${buttons}</div>`;
+        buttons = '';
+    }
 
-        });
-        k++;
-
-    });
-
-
+    wrapper.innerHTML = rows;
 }
 
 createLayout(enAlphabet);
