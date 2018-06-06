64bef6da156446af160da2731d45a7f197757a02
Alex_vlasov1988 <Alex_vlasov@i.ua>
2018-06-06 11:53:24 +0300

    4th commit

--- Module-14JS/script.js
+++ Module-14JS/script.js
@@ -1,19 +1,6 @@
-/*
-1 Создать две кнопки в HTML: start и stop.
-
-2 Реализовать функционал таймера отсчета старта
- печати через функцию-конструктор со свойсвами startTime,
-  stopTime и interval. Добавить в prototype методы start
-   и stop.
-3 При нажатии на кнопку start, функция сохраняет момент
- нажатия в свойство startTime.
-4 При нажатии на кнопку stop, функция сохраняет значение
- текущего момента времени в stopTime и записывает разницу 
- между startTime и stopTime в interval.
-5 При нажатии на stop, значение interval выводится в консоль.*/
-
 const startBtn = document.querySelector('.str');
 const stopBtn = document.querySelector('.stp');
+
 
 function Timer(startTime, stopTime, interval) {
     this.startTime = startTime;
@@ -22,22 +9,18 @@ function Timer(startTime, stopTime, interval) {
 }
 
 Timer.prototype.start = function() {
-    this.startTime = getCurrentTime();
+    this.startTime = new Date();
 
 }
 
 Timer.prototype.stop = function() {
-    this.stopTime = getCurrentTime();
-    let timeArr = [];
-    for (i = 0; i < this.startTime.length; i++) {
-        timeArr.push(this.stopTime[i] - this.startTime[i])
-    }
-    this.interval = timeArr;
-    console.log(`Интервал времени ${this.interval[0]} минут
-     и ${this.interval[1]} секунд `);
+    this.stopTime = new Date();
+    this.interval = this.stopTime - this.startTime;
+    console.log(`interval : ${this.interval} ms`)
+
 }
 
-let timeStart = new Timer();
+let timeStart = new Timer(0, 0, 0);
 
 startBtn.addEventListener('click', () => {
     timeStart.start()
@@ -45,11 +28,3 @@ startBtn.addEventListener('click', () => {
 stopBtn.addEventListener('click', () => {
     timeStart.stop()
 })
-
-function getCurrentTime() {
-    let now = new Date();
-    let startMin = now.getMinutes();
-    let startSec = now.getSeconds();
-    return [startMin, startSec];
-
-}
