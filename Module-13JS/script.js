df23615ac7622a5e361b0a1f41393597ccf5d770
Alex_vlasov1988 <Alex_vlasov@i.ua>
2018-06-08 10:38:53 +0300

    Second commit

--- Module-13JS/script.js
+++ Module-13JS/script.js
@@ -9,18 +9,20 @@ let validateStatus = {};
 submitBtn.addEventListener("click", validate);
 
 function validate(evt) {
+    resultsList.innerHTML = '';
     evt.preventDefault();
-    // inputs
+
     let firstInput = firstName.value;
     let lastInput = lastName.value;
     let telInput = tel.value;
 
-    let chackingName = /^[a-zа-яё]{1,}(?: [a-zа-яё]{1,}){0,2}?$/i;
-    let chackingSureName = /^[a-zа-яё_-]{1,}(?: [a-zа-яё]{1,})?$/i;
+    let chackingName = /^[a-zа-яё]{3,20}(?: [a-zа-яё]{3,20}){0,2}?$/i;
+    let chackingSureName = /^[a-zа-яё_-]{3,20}(?: [a-zа-яё]{3,20})?$/i;
     let chakingTel = /^\+?(38)?0{1}[1-9]{1}[0-9]{8}$/;
     validateStatus.firstNameStatus = chackingName.test(firstInput);
     validateStatus.lastNameStatus = chackingSureName.test(lastInput);
     validateStatus.telStatus = chakingTel.test(telInput);
+    clear();
     showResults(validateStatus);
 }
 
@@ -30,4 +32,11 @@ function showResults(results) {
             resultsList.innerHTML += '<li class ="success">SUCCESS: ' + key + ' passed validation </li>';
         } else resultsList.innerHTML += '<li class ="error">ERROR: ' + key + ' failed validation </li>';
     }
+}
+
+function clear() {
+    firstName.value = '';
+    lastName.value = '';
+    tel.value = '';
+
 }
